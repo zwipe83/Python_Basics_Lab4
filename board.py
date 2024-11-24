6e0937e0956ce827316ab3dcef3ee2ad8b5ec86f
Samuel Jeffman <sjeffman83@outlook.com>
2024-11-24 11:20:21 +0100

    Various changes. Added assertions. Working, tested and not optimized.

--- board.py
+++ board.py
@@ -1,4 +1,9 @@
-# FILL THIS FILE
+"""
+File Name: board.py
+Author: Samuel Jeffman
+Date: 2024-11-24
+Description: Programming in Python: Basic and Preparatory Course, Assignment 4. Tic-tac-toe app.
+"""
 
 # Create new board, 3x3 empty('-') cells
 def new_board():
@@ -10,21 +15,35 @@ def new_board():
         board.append(new_row)
     return board
 
+assert new_board() ==   [['-','-','-'],
+                         ['-','-','-'],
+                         ['-','-','-']]
+
 # Return a specific position in the matrix
 def get(board, row, col):
     return board[row][col]
+
+assert get(new_board(), 0, 0) == '-'
+assert get(new_board(), 2, 2) == '-'
 
 # Returns True if cell is empty('-')
 def is_empty(board, row, col):
     return board[row][col] == '-'
 
+assert is_empty(new_board(), 0,0) == True
+assert is_empty(new_board(), 2,2) == True
+
 # Returns the width of the board
 def get_width(board):
     return len(board[0])
 
+assert get_width(new_board()) == 3
+
 # Returns the height of the board
 def get_height(board):
     return len(board)
+
+assert get_height(new_board()) == 3
 
 # Prints the board to screen
 def print_board(board):
@@ -32,6 +51,7 @@ def print_board(board):
         for col in range(get_width(board)):
             print(board[row][col], end=' ')
         print('')
+    print('\n')
     return 0
 
 # Place a marker at a specific position if it is empty, otherwise return False
@@ -41,6 +61,9 @@ def place(board, marker, row, col):
         return True
     return False
 
+assert place(new_board(), 'X', 0,0) == True
+assert place(new_board(), 'O', 1,1) == True
+
 # Returns True if there are no empty cells left
 def is_full(board):
     for row in range(get_height(board)):
@@ -49,6 +72,8 @@ def is_full(board):
                 return False
     return True
 
+assert is_full(new_board()) == False
+
 # Check if specific "marker" is winner or not.
 # Checks for 3 in a row, column or diagonal
 def is_winner(board, marker):
@@ -56,10 +81,10 @@ def is_winner(board, marker):
     width = get_width(board)
     length = 3
 
-    # Check ros/cols
-    row_count = 0
-    col_count = 0
+    # Check rows/cols
     for row in range(height):
+        row_count = 0
+        col_count = 0
         for col in range(width):
             # Check rows
             if board[row][col] == marker:
@@ -77,12 +102,10 @@ def is_winner(board, marker):
             else:
                 col_count = 0
     # Check diagonal
-    diag_count1 = 0
-    diag_count2 = 0
     if height/width == 1: # Only possible if matrix is square
+        diag_count1 = 0
+        diag_count2 = 0
         for row in range(height):
-
-            # Search diagonally \
             col = row
             if board[row][col] == marker:
                 diag_count1 += 1
@@ -90,9 +113,8 @@ def is_winner(board, marker):
                     return True
             else:
                 diag_count1 = 0
-            
-            # Search diagonally /
-            col = height - row - 1
+
+            col = height - 1 - row
             if board[row][col] == marker:
                 diag_count2 += 1
                 if diag_count2 >= length:
@@ -100,3 +122,32 @@ def is_winner(board, marker):
             else:
                 diag_count2 = 0
     return False
+
+board1 = new_board()
+place(board1,'X', 0, 1)
+place(board1,'X', 1, 1)
+place(board1,'X', 2, 1)
+assert is_winner(new_board(), 'X') == False
+assert is_winner(board1, 'X') == True
+assert is_winner(board1, 'O') == False
+
+if __name__ == '__main__':
+    #... Test code for the functions, for example ...
+    board2 = new_board()
+    place(board2, 'X', 0, 0)
+    print_board(board2)
+    place(board2, 'O', 0, 0)
+    place(board2, 'O', 1, 0)
+    print_board(board2)
+    place(board2, 'X', 1, 1)
+    print_board(board2)
+    place(board2, 'O', 2, 2)
+    print_board(board2)
+    place(board2, 'X', 0, 1)
+    print_board(board2)
+    place(board2, 'O', 1, 2)
+    print_board(board2)
+    place(board2, 'X', 2, 1)
+    print_board(board2)
+    print("O wins?","Yes" if is_winner(board2, 'O') else "No")
+    print("X wins?","Yes" if is_winner(board2, 'X') else "No")
